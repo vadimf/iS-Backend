67b3cda78a4dba131a1c46784501f7d6cc905701
maty <maty@hookiz.com>
2018-06-12 12:24:47 +0300

    Testing executable child_process

--- src/controllers/system/system.ts
+++ src/controllers/system/system.ts
@@ -1,8 +1,10 @@
 import * as express from "express";
 import { SystemConfiguration } from "../../models/system-vars";
-import * as buffer from "buffer";
+// import * as buffer from "buffer";
 // import { spawn } from "child_process";
-const ffmpeg = require("ffmpeg");
+// const ffmpeg = require("ffmpeg");
+const ffmpeg = require("fluent-ffmpeg");
+
 
 
 const router = express.Router();
@@ -25,7 +27,7 @@ router.get("/", (req: express.Request, res: express.Response) => {
     // const ls = spawn("ffmpeg", ["-i", "/home/maty/server/ge7-vf3rlt2ucusk0lo8j4zc.mp4", "pipe:1"]);
     //
     // ls.stdout.on("data", (data) => {
-    //     console.log(`stdout: ${data}`);
+    //     console.log(`stdout: ${data}`);npm
     // });
     //
     // ls.stderr.on("data", (data) => {
@@ -36,34 +38,40 @@ router.get("/", (req: express.Request, res: express.Response) => {
     //     console.log(`child process exited with code ${code}`);
     // });
 
-    try {
-        const process = new ffmpeg(file);
-        process.then((video: any) => {
-            // video
-            //     .format("gif")
-            //     .size("640x360")
-            //     .duration("0:15")
-            //     .inputFPS(8)
-            //     .save("/home/maty/server/dist/public/images/gif.gif", (error: any, file: any) => {
-            //         if ( ! error ) {
-            //             console.log("Video file: " + file);
-            //         }
-            //         console.log("save error", error);
-            //     });
+    // try {
+    //     const process = new ffmpeg(file);
+    //     process.then((video: any) => {
+    //         // video
+    //         //     .format("gif")
+    //         //     .size("640x360")
+    //         //     .duration("0:15")
+    //         //     .inputFPS(8)
+    //         //     .save("/home/maty/server/dist/public/images/gif.gif", (error: any, file: any) => {
+    //         //         if ( ! error ) {
+    //         //             console.log("Video file: " + file);
+    //         //         }
+    //         //         console.log("save error", error);
+    //         //     });
+    //
+    //         console.log(video);
+    //     });
+    // }
+    // catch (e) {
+    //     console.log("e", e);
+    // }
 
-            console.log(video);
-        });
-    }
-    catch (e) {
-        console.log("e", e);
-    }
-
-    // ffmpeg()
-    //     .format("gif")
-    //     .size("640x360")
-    //     .duration("0:3")
-    //     .inputFPS(15)
-    //     .writeToStream(outStream, { end: true });
+    ffmpeg(file)
+        .format("gif")
+        .size("620x?")
+        .duration("320")
+        .inputFPS(15)
+        .on("error", (err: any) => {
+            console.log("An error occurred: " + err.message);
+        })
+        .on("end", (f: any) => {
+            console.log("Processing finished !", f);
+        })
+        .save("/home/maty/server/public/images/gif.gif");
 });
 
 
