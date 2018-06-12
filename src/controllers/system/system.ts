9bf06f2dd27f2427f284fbb0a47f10b6b52b89ab
maty <maty@hookiz.com>
2018-06-12 11:16:28 +0300

    Testing executable child_process

--- src/controllers/system/system.ts
+++ src/controllers/system/system.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { SystemConfiguration } from "../../models/system-vars";
+import { spawn } from "child_process";
 
 const router = express.Router();
 
@@ -14,6 +15,20 @@ const router = express.Router();
 router.get("/", (req: express.Request, res: express.Response) => {
     res.response({
         vars: SystemConfiguration.toJson()
+    });
+
+    const ls = spawn("ls", ["-lh", "/usr"]);
+
+    ls.stdout.on("data", (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on("data", (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
     });
 });
 
